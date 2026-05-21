3f57317bbb287e185f3ed6a9ad47293aa75bdaa8
Shreyen Reddy <2451299@students.wits.ac.za>
2026-05-21 12:34:06 +0200

    about me page almost done

---
+++ main.js
@@ -0,0 +1,30 @@
+// //Card Data 
+// const cardData= [
+//     {
+//         heading: "Biography",
+//         text: "Hi, Im Shreyen Reddy!"
+//     },
+//     {
+//         heading:"Contact Info ",
+//         text:" Here is my contact info"
+//     }
+// ];
+
+// //Create and Append each card
+// const container= document.querySelector("#card-container");
+
+// cardData.forEach(item=>
+//     {
+//         const card= document.createElement("div");
+//         card.classList.add("card");
+
+//         const heading= document.createElement("h2");
+//         heading.textContent=item.heading;
+
+//         const paragraph=document.createElement("p");
+//         paragraph.textContent=item.text;
+
+//         card.appendChild(heading);
+//         card.appendChild(paragraph);
+//         container.appendChild(card);
+//     });
